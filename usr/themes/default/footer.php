af70ffdc8c37af5f98621b6ae80397f976f7943c
Morse <AshleyWilliams2239@outlook.com>
2026-07-11 13:05:38 +0800

    Update footer.php

--- usr/themes/default/footer.php
+++ usr/themes/default/footer.php
@@ -1,8 +1,4 @@
-<footer id="footer" role="contentinfo">
-    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    <?php _e('由 Cloudflare 强力驱动'); ?>.
-</footer><!-- end #footer -->
-<div style="text-align:center; font-size:14px; margin:0.3rem 0; padding:0;">
+<div style="text-align:center; font-size:14px; margin:0; padding:0;">
     <a href="/privacy">隐私政策</a> |
     <a href="/statement">免责声明</a> |
     <a href="/sitemap.html">站点地图</a>
@@ -13,6 +9,11 @@
     echo " | 本站已稳定运行：{$diff->y}年{$diff->m}月{$diff->d}天";
     ?>
 </div>
+
+<footer id="footer" role="contentinfo" style="margin:0; padding:0;">
+    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
+    <?php _e('由 Cloudflare 强力驱动'); ?>.
+</footer><!-- end #footer -->
 
 <?php $this->footer(); ?>
 </body>
