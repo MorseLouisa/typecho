7545d9cb82f08068ee4494d0eb4e9f9a922cbb8f
Morse <AshleyWilliams2239@outlook.com>
2026-07-11 14:32:31 +0800

    Update Plugin.php

--- usr/plugins/Sitemap/Plugin.php
+++ usr/plugins/Sitemap/Plugin.php
@@ -1,1 +1,25 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * Sitemap 站点地图
+ *
+ * @package Sitemap
+ * @author Typecho
+ * @version 1.0
+ * @link https://typecho.org
+ */
+class Sitemap_Plugin implements Typecho_Plugin_Interface
+{
+    public static function activate()
+    {
+        Typecho_Router::set('sitemap', '/sitemap.xml', 'Sitemap_Action', 'render');
+    }
 
+    public static function deactivate()
+    {
+        Typecho_Router::delete('sitemap');
+    }
+
+    public static function config(Typecho_Widget_Helper_Form $form){}
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+}
